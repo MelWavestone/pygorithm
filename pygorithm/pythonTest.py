1a6c54815c7432d883e1f8a93ec10c858a7cec8a
MelWavestone <30470555+MelWavestone@users.noreply.github.com>
2017-08-29 15:47:09 +0200

    Update pythonTest.py

--- pygorithm/pythonTest.py
+++ pygorithm/pythonTest.py
@@ -1,4 +1,4 @@
-print("Veuillez entrer un nombre positif quelconque : ", end=" ")
+print("Veuillez entrer un nombre positif quelconque : ")
 ch = input()
 nn = int(ch)	     # conversion de la chaîne en un nombre entier
 print("Le carré de", nn, "vaut", nn**2)
